ad1818726f89d511133a740a04c72f1274b677b2
Kuleb9lka <bogdan.lopatenko@gmail.com>
2022-10-10 19:17:22 +0300

    filled roles and positions tables

---
+++ server/truckplast-analyzer/src/main/resources/db/changelog/v.0.2/sql/fill-positions-table.sql
@@ -0,0 +1,9 @@
+INSERT INTO positions (id, name) VALUES (1, 'DIRECTOR');
+INSERT INTO positions (id, name) VALUES (2, 'SUPERVISOR');
+INSERT INTO positions (id, name) VALUES (3, 'MANAGER');
+INSERT INTO positions (id, name) VALUES (4, 'DRIVER');
+INSERT INTO positions (id, name) VALUES (5, 'STOREKEEPER');
+INSERT INTO positions (id, name) VALUES (6, 'EMPLOYEE');
+INSERT INTO positions (id, name) VALUES (7, 'BOOKKEEPER');
+INSERT INTO positions (id, name) VALUES (8, 'LOGIST');
+INSERT INTO positions (id, name) VALUES (9, 'WAREHOUSE MANAGER');
