a71940676ffe4a9fd9f6f749bdcecccb0bee8a45
Kuleb9lka <bogdan.lopatenko@gmail.com>
2022-10-26 23:00:38 +0300

    added oneToMany annotation

--- server/truckplast-analyzer/src/main/java/com/truckplast/analyzer/entity/Company.java
+++ server/truckplast-analyzer/src/main/java/com/truckplast/analyzer/entity/Company.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "companies")
@@ -19,5 +20,8 @@ public class Company {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.DETACH})
+    private Set<User> users;
 
 }
